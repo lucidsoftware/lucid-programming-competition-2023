ede5f660892974e5deaf6635e9f6aa5b0876ac00
Tajen Freeman <tfreeman@lucidchart.com>
2023-09-22 13:56:01 -0600

    Fixed solution algorithm, added more stress tests

--- problems/TerritoryDispute/solutions/python3/main.py
+++ problems/TerritoryDispute/solutions/python3/main.py
@@ -2,16 +2,21 @@
 count, dinos, public_need = map(int, input().split(' '))
 territories = [int(input()) for _ in range(count)]
 
-#get total size of land to check viability of specific subsets
-total_land = sum(territories)
-
 #sort list for sliding window
 territories.sort()
+
+#get total land size at every sorted index
+total_land = 0
+territories_sum = [0]
+for i in range(count):
+    total_land += territories[i]
+    territories_sum.append(total_land)
+
 best = -1
 #find best territory with a sliding window
 for i in range(count-dinos+1):
     #subtract potential land used by dinos from the total to check viability
-    available_land = total_land - sum(territories[i:i+dinos])
+    available_land = total_land - territories_sum[i+dinos] + territories_sum[i]
     if (available_land >= public_need):
         #find the difference and compare it to previous best
         diff = territories[i+dinos-1] - territories[i]
