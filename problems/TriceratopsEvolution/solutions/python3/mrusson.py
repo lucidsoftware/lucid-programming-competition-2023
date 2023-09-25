a983bb07a3cf5ffe01b70c564d8943cbf71a8a43
Matthew Russon <mrusson@lucidchart.com>
2023-09-25 17:27:14 -0600

    Solve TriceratopsEvolution

---
+++ problems/TriceratopsEvolution/solutions/python3/mrusson.py
@@ -0,0 +1,15 @@
+items = input().split(' ')
+n = int(items[0])
+g1 = float(items[1])
+p0 = float(items[2])
+p1 = float(items[3])
+p2 = float(items[4])
+
+g = g1
+for i in range(1, n):
+    none = (1 - g) * (1 - g) * p0
+    one = g * (1 - g) * p1 * 2
+    two = g * g * p2
+    g = none + one + two
+
+print("%.4f" % round(g, 4))
